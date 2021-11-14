32647ba5e761f18c3ee1a2ff46d4322d686dba8b
Artem <art.teselcko@yandex.ru>
2021-11-14 21:35:25 +0300

    commit

--- src/main/java/homework5/employee.java
+++ src/main/java/homework5/employee.java
@@ -1,4 +1,25 @@
 package homework5;
 
 public class employee {
+    private String full_name;
+    private String post;
+    private String email;
+    private int telephone;
+    private int salary;
+    private int age;
+
+    public employee(String full_name,String post,String email,int telephone,int salary,int age){
+        this.full_name = full_name;
+        this.post = post;
+        this.email = email;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void info() {
+        System.out.printf("full_name: %s, post: %s, email: %s, telephone: %d, salary: %d, age: %d\n", this.full_name, this.post, this.email, this.telephone, this.salary, this.age);
+    }
+    public int getAge() {
+        return this.age;
+    }
 }
--- src/main/java/homework5/helping.java
+++ src/main/java/homework5/helping.java
@@ -1,4 +1,18 @@
 package homework5;
 
 public class helping {
+    public static void main(String[] args) {
+        employee[] employees = {   // заралпта в белорусских рублях и телефон без кода делал
+                new employee("Artem A.I.", "Engineer", "artem@mail.ru", 7242884, 1000, 38),
+                new employee("Ivan I.I.", "Welder", "ivan@mail.ru", 7853401, 1500, 25),
+                new employee("Sasha L.A.", "Installer", "sasha@mail.ru", 5482945, 800, 48),
+                new employee("Igor O.P.", "Hard worker", "igor@mail.ru", 5333798, 500, 43),
+                new employee("Yriy K.K", "Programmer", "yriy@mail.ru", 6789853, 3200, 23),
+        };
+        for (employee x:  // делал через foreach прост если надо по всем пробежаться им проще не знаю правильно или нет
+             employees) {
+            if(x.getAge() > 40)
+                x.info();
+        }
+    }
 }
